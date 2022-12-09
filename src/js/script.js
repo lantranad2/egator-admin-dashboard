6669ac63f13bf2a5b67a4499a6966f7828db9f82
LanAd2 <tranthanhlan.ad2@gmail.com>
2022-12-09 17:14:57 +0700

    Implement light and dark mode.

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,9 @@
 const navIconMenu = document.querySelector("#nav__icon-menu");
 const asideIconClose = document.querySelector("#aside__icon-close");
 const aside = document.querySelector("#aside");
+const navLightMode = document.querySelector("#nav__light-mode");
+const navDarkMode = document.querySelector("#nav__dark-mode");
+const body = document.body;
 
 navIconMenu.addEventListener("click", () =>
   aside.classList.add("aside--expanded")
@@ -9,3 +12,15 @@ navIconMenu.addEventListener("click", () =>
 asideIconClose.addEventListener("click", () =>
   aside.classList.remove("aside--expanded")
 );
+
+navLightMode.addEventListener("click", () => {
+  navLightMode.classList.add("active");
+  navDarkMode.classList.remove("active");
+  body.classList.remove("dark-mode");
+});
+
+navDarkMode.addEventListener("click", () => {
+  navDarkMode.classList.add("active");
+  navLightMode.classList.remove("active");
+  body.classList.add("dark-mode");
+});
